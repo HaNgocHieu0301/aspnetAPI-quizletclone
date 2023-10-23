5ba54afa7dc8fa1884281607b47c66b70e8e8a58
hieuhn0301 <hieuhn0301@gmail.com>
2023-10-23 09:12:22 +0700

    Revert "add AuthAPI"
    This reverts commit a684ae085b1268ff94178c3eaff69666bae1a518.
    
    # Conflicts:
    #	.vs/PRN231_FinalProject/DesignTimeBuild/.dtbcache.v2
    #	.vs/PRN231_FinalProject/v17/.futdcache.v2
    #	.vs/PRN231_FinalProject/v17/.suo
    #	Quizlet.Services.AuthAPI/obj/Debug/net6.0/Quizlet.Services.AuthAPI.assets.cache
    #	Quizlet.Services.AuthAPI/obj/Debug/net6.0/Quizlet.Services.AuthAPI.csproj.AssemblyReference.cache
    #	Quizlet.Services.AuthAPI/obj/Quizlet.Services.AuthAPI.csproj.nuget.dgspec.json
    #	Quizlet.Services.AuthAPI/obj/Quizlet.Services.AuthAPI.csproj.nuget.g.props
    #	Quizlet.Services.AuthAPI/obj/project.assets.json
    #	Quizlet.Services.AuthAPI/obj/project.nuget.cache

--- Quizlet.Services.AuthAPI/Models/DTO/ResponseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Quizlet.Services.AuthAPI.Models.DTO
-{
-    public class ResponseDTO
-    {
-        public object? Result { get; set; }
-        public bool IsSuccess { get; set; } = true;
-        public string Message { get; set; } = "";
-    }
-}
--- Quizlet.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Quizlet.Services.AuthAPI.Models;
-
-namespace Quizlet.Services.AuthAPI.Service.IService
-{
-    public interface IJwtTokenGenerator
-    {
-        string GeneratorToken(ApplicationUser user, IEnumerable<string> roles);
-    }
-}
