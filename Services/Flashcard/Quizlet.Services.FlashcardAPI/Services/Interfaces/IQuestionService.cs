3f5821fca506d635c947269ddb2a3354e3440276
hieuhn0301 <hieuhn0301@gmail.com>
2023-11-13 07:13:28 +0700

    update api

---
+++ Services/Flashcard/Quizlet.Services.FlashcardAPI/Services/Interfaces/IQuestionService.cs
@@ -0,0 +1,9 @@
+﻿using BusinessObject.DTOs;
+
+namespace BusinessObject.Services.Interfaces;
+
+public interface IQuestionService
+{
+    public IEnumerable<QuestionDTO> GetQuestionsByLessonId(int lessonId);
+    void UpdateRangeQuestion(List<EditQuestionDTO> questionDtOs);
+}
--- Services/Flashcard/Repository/IRepository/IQuestionRepository.cs
+++ Services/Flashcard/Repository/IRepository/IQuestionRepository.cs
@@ -8,9 +8,11 @@ namespace Repository.IRepository
         List<QuestionDTO> GetQuestions();
         int AddQuestion(AddQuestionDTO addQuestionDTO);
         List<Question> AddRangeQuestion(List<AddQuestionDTO> addQuestionDTOs);
+        List<Question> AddRangeQuestion(List<QuestionDTO> addQuestionDTOs);
         void UpdateQuestion(int id, EditQuestionDTO editQuestionDTO);
         void DeleteQuestion(int id);
         void DeleteRangeQuestion(List<QuestionDTO> questions);
         void UpdateRangeQuestion(List<QuestionDTO> questionDTOs);
+        void UpdateRangeQuestion(List<EditQuestionDTO> questionDTOs);
     }
 }
